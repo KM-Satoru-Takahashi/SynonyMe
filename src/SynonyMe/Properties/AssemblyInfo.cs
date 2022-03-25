8c84e631802e8f99edafc4ec6c0c3932346badfc
KM-Satoru-Takahashi <stakahashi@liberworks.co.jp>
2022-03-25 10:45:43 +0900

    v1.0.0.0作成

--- src/SynonyMe/Properties/AssemblyInfo.cs
+++ src/SynonyMe/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("RepeatChecker")]
+[assembly: AssemblyTitle("SynonyMe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RepeatChecker")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("SynonyMe")]
+[assembly: AssemblyCopyright("Copyright © Satoru Takahashi 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
